45949b0edcaf6cc0ee15891bb17aca0316cfdff6
srikanthpragada <srikanthpragada@gmail.com>
2019-12-27 10:55:49 +0530

    27-DEC

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30, unique=True)
+    mobile = models.CharField(max_length=10, null=True)
+    remarks = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name + "," + self.email
